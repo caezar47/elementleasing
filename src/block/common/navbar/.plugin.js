eaaf0be429b5b00c3350a329bc71dc62beb7f8bd
caezar47 <iss47@ya.ru>
2020-02-26 07:54:26 +0300

    Сверстал header и footer

---
+++ src/block/common/navbar/.plugin.js
@@ -0,0 +1,20 @@
+var query = document.querySelector.bind(document);
+var body = document.querySelector('body');
+
+var humb = document.querySelector('[data-hamburger]');
+humb.addEventListener('click',  function(e) {
+	e.preventDefault();
+	var collapse = query('.navbar__collapse');
+	var html = query('html');
+	collapse.classList.toggle('is--open');
+	html.classList.toggle('is--open-navbar');
+	this.classList.toggle('is--active');
+});
+/*
+body.addEventListener('click',  function(e) {
+	if(e.target.closest('.navbar__block').length == 0){
+
+		alert('1');	
+	}
+});
+*/
