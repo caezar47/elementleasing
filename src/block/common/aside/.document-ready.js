697f0123198f6237d6abb389ee31baa0b13004f0
caezar47 <iss47@ya.ru>
2020-02-27 09:54:31 +0300

    Сверстал страницу Руководство

--- src/block/common/aside/.document-ready.js
+++ src/block/common/aside/.document-ready.js
@@ -1,3 +1,4 @@
 var url = window.location.pathname;
 //var url = window.location.href;
 $('.aside__nav a[href="'+url+'"]').addClass('is--active'); 
+$('.aside__block').closest('body').addClass('is--aside-bottom'); 
