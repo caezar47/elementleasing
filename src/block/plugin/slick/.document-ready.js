0e9f12259e3ba821fcd4c2796c36a1eae5f83c98
caezar47 <iss47@ya.ru>
2020-03-25 08:50:14 +0300

    Исправил слайдеры на главной

--- src/block/plugin/slick/.document-ready.js
+++ src/block/plugin/slick/.document-ready.js
@@ -1,19 +1,36 @@
 'use strict';
 $(function() { 
+	var header = $('[data-slick-header]');
 	var catalog = $('[data-slick-catalog]');
 	var partners = $('[data-slick-partners]');
 	var finance = $('[data-slick-finance]');
+	var leasing = $('[data-slick-leasing]');
 	var dots = $('[data-slick-dots]').data('slick-dots');
 	//console.log(dots);
 
-	var header = $('[data-slider-slick="slick-header"]');	
-	var gallery = $('[data-slider-slick="slick-gallery"]');	
-	var CMS__TPL_PATH = '/wp-content/themes/azbn7theme';  
-	//var CMS__TPL_PATH = '/tpl';  
-	var CMS__TPL_PATH = '';  
-	//local
 	var prevArrow = '<button type="button" class="slick-btn  is--prev"><span class="sr-only">Предыдущий слайд</span></button>';
-	var nextArrow = '<button type="button" class="slick-btn  is--next"><span class="sr-only">Следующий слайд</span></button>';	
+	var nextArrow = '<button type="button" class="slick-btn  is--next"><span class="sr-only">Следующий слайд</span></button>';
+
+	var count = $('.slick-count');
+    
+
+    header.on('init reInit afterChange', function(event, slick, currentSlide, nextSlide) {
+          var i = (currentSlide ? currentSlide : 0) + 1;
+          $(this).find(count).html('<span>'+i + '</span><span>/</span><span>' + slick.slideCount+'</span>');
+    });
+	header.slick({
+		slide: '.content-block__slider-slide',
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		infinite: true,
+		arrows: true,
+		dots: false,
+		autoplay: true,
+  		autoplaySpeed: 4000,
+		prevArrow: prevArrow,
+		nextArrow: nextArrow,
+		fade: true
+	});	
 	
 	catalog.slick({
 		slidesToShow: 2,
@@ -28,10 +45,48 @@ $(function() {
   		//autoplaySpeed: 4000,
 		prevArrow: prevArrow,
 		nextArrow: nextArrow,
+		// responsive: [
+		//     {
+		// 		breakpoint: 768,				
+		//       	settings: "unslick"
+		//     }
+		// ]
 		responsive: [
 		    {
-				breakpoint: 768,				
-		      	settings: "unslick"
+				breakpoint: 576,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					dots: false,
+				}
+		    }
+		]
+	});
+	leasing.slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		dots: false,
+		infinite: false,
+		responsive: [
+		    {
+				breakpoint: 3000,
+					settings: "unslick"
+				
+		    },
+		    {
+				breakpoint: 767,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1, 
+				}
+		    },
+		    {
+				breakpoint: 576,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1, 
+				}
 		    }
 		]
 	});
@@ -102,46 +157,8 @@ $(function() {
 	  }
 	});*/
 
-	header.slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		infinite: true,
-		arrows: false,
-		dots: false,
-		autoplay: true,
-  		autoplaySpeed: 4000,
-		//prevArrow: prevArrow,
-		//nextArrow: nextArrow,
-		fade: true
-	});
-	gallery.slick({
-		slidesToShow: 4,
-		slidesToScroll: 4,
-		arrows: true,
-		dots: true,
-		infinite: true,
-		prevArrow: prevArrow,
-		nextArrow: nextArrow,
-		responsive: [
-		    {
-				breakpoint: 1600,
-				settings: {
-					slidesToShow: 3,
-					slidesToScroll: 3,
-				}
-		    },
-		    {
-				breakpoint: 768,
-				settings: {
-					dots: false,
-					slidesToShow: 1,
-					slidesToScroll: 1,
-				}
-		    }
-		]
-	});
-
 	
-	$('.slick-cloned .content-block__preview').removeAttr('data-fancybox');
-	$('.slick-cloned .card-item__preview').removeAttr('data-fancybox');
+	
+	//$('.slick-cloned .content-block__preview').removeAttr('data-fancybox');
+	//$('.slick-cloned .card-item__preview').removeAttr('data-fancybox');
 }); 
